a045cf98aaa3e74ddae5b1aa6e854f105392224e
Bartek <bartekp44@gmail.com>
2016-12-01 21:35:26 +0100

    Create Entertainment project. Add basic classes and empty window.

---
+++ Hotel/EntertainmentApp/InnerEntertainment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntertainmentApp
+{
+    public class InnerEntertainment : Entertainment
+    {
+
+        
+
+        public double computePayment(int hoursSpent )
+        {
+            return price * hoursSpent;
+        }
+
+        public string printReceipt()
+        {
+            return "Entertainment name: " + name + " \nPrice: " + price;
+        }
+
+    }
+}
