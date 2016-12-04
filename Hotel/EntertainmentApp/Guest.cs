595bb1d72651c92243e44fcce8b5a7d25c96708a
HboHD <slaskiezakladyazotowe@gmail.com>
2016-12-04 20:00:12 +0100

    Add temporary Guest and Employee classes

---
+++ Hotel/EntertainmentApp/Guest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntertainmentApp
+{
+    class Guest
+    {
+        private string name { get;set;}
+        private string surname { get; set; }
+        private int ID { get; set; }
+    }
+}
