15b149905943a519fd40bdd177ee892625f63c0a
HboHD <slaskiezakladyazotowe@gmail.com>
2016-12-04 20:49:10 +0100

    Entertainment Window prototype

--- Hotel/EntertainmentApp/EntertainmentWindow.xaml.cs
+++ Hotel/EntertainmentApp/EntertainmentWindow.xaml.cs
@@ -20,9 +20,30 @@ namespace EntertainmentApp
     /// </summary>
     public partial class EntertainmentWindow : Window
     {
+        private List<Entertainment> m_EntertainmentList = null;
         public EntertainmentWindow()
         {
             InitializeComponent();
+            InitBinding();
         }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OrganisedEvents_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+    private void InitBinding()
+    {
+            m_EntertainmentList = new List<Entertainment>();
+            m_EntertainmentList.Add(new OrganisedEvent(1, "Jan", "Kowalski", 25));
+            m_EntertainmentList.Add(new OrganisedEvent(2, "Adam", "Nowak", 24));
+            m_EntertainmentList.Add(new OrganisedEvent(3, "Agnieszka", "Kowalczyk", 23));
+            OrganisedEvents.ItemsSource = m_EntertainmentList;
     }
+}
 }
--- Hotel/EntertainmentApp/OrganisedEvent.cs
+++ Hotel/EntertainmentApp/OrganisedEvent.cs
@@ -1,16 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EntertainmentApp
 {
     class OrganisedEvent : Entertainment
     {
-        private int maximumNumberOfGuests { get; set; }
-        private DateTime startDate { get; set; }
-        private DateTime endDate { get; set; }
+        public OrganisedEvent(string name, float price, int maximumNumberOfGuests,
+            DateTime startDate, DateTime endDate, Employee supervisor)
+        {
+            this.name = name;
+            this.price = price;
+            this.maximumNumberOfGuests = maximumNumberOfGuests;
+            this.startDate = startDate;
+            this.endDate = endDate;
+            this.supervisor = supervisor;
+        }
+
+        public int maximumNumberOfGuests { get; set; }
+        public DateTime startDate { get; set; }
+        public DateTime endDate { get; set; }
         public Employee supervisor { get; set; }
         public List<Guest> guestList { get; set; }
 
