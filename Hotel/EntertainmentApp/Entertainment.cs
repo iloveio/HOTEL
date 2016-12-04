5a932acdefb296a04526fefa9f2c23cd87749fc3
HboHD <slaskiezakladyazotowe@gmail.com>
2016-12-04 20:11:19 +0100

    Minor improvement in Entertainment class, time for UI

--- Hotel/EntertainmentApp/Entertainment.cs
+++ Hotel/EntertainmentApp/Entertainment.cs
@@ -10,7 +10,15 @@ namespace EntertainmentApp
     {
         protected float price { get; set; }
         protected string name { get; set; }
+        protected List<Employee> employeeList { get; set; }
 
-        
+        public void addEmployeeToShift(Employee employee)
+        {
+            employeeList.Add(employee);
+        }
+        public void removeEmployeeFromShift(Employee employee)
+        {
+            employeeList.Remove(employee);
+        }
     }
 }
