a3d6a17c9f93097bda3edeb105f256d0ca433f95
HboHD <slaskiezakladyazotowe@gmail.com>
2016-12-07 18:18:15 +0100

    Left alone at home class OurData wants to be in repo as well

---
+++ Hotel/EntertainmentApp/OurData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntertainmentApp
+{
+    public class OurData
+    {
+        public OurData()
+        {
+            m_OrganisedEvents = new ObservableCollection<OrganisedEvent>();
+            m_InnerEntertainments = new ObservableCollection<InnerEntertainment>();
+        }
+
+        public ObservableCollection<OrganisedEvent> m_OrganisedEvents { get; set; }
+        public ObservableCollection<InnerEntertainment> m_InnerEntertainments { get; set; }
+
+        public void addToCollection(OrganisedEvent organisedEvent)
+        {
+            m_OrganisedEvents.Add(organisedEvent);
+        }
+        public void addToCollection(InnerEntertainment innerEntertainment)
+        {
+            m_InnerEntertainments.Add(innerEntertainment);
+        }
+        public void removeFromCollection(OrganisedEvent organisedEvent)
+        {
+            m_OrganisedEvents.Remove(organisedEvent);
+        }
+        public void removeFromCollection(InnerEntertainment innerEntertainment)
+        {
+            m_InnerEntertainments.Remove(innerEntertainment);
+        }
+    }
+}
