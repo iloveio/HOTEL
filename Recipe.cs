0560fc78c4467ade7df13d499ab4134586bd5d61
K_Kuba <195623@edu.p.lodz.pl>
2016-12-09 11:31:36 +0100

    Add files via upload

---
+++ Recipe.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kitchen
+{
+    class Recipe
+    {
+        public Recipe(string name, List<string> ingredientNames )
+        {
+            this.name = name;
+            this.ingredientNames = new List<string>(ingredientNames);
+        }
+
+        public string Return_Name()
+        {
+            return this.name;
+        }
+
+        public List<string> Return_ingredientNames()
+        {
+            return this.ingredientNames;
+        }
+
+        private string name;
+        private List<string> ingredientNames;
+    }
+}
