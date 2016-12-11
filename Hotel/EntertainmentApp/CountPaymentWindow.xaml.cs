5dc6aea790768c17d61fecdf5b319ae8a8800dc5
HboHD <slaskiezakladyazotowe@gmail.com>
2016-12-11 20:38:37 +0100

    'Printing' receipt is purrfect now

--- Hotel/EntertainmentApp/CountPaymentWindow.xaml.cs
+++ Hotel/EntertainmentApp/CountPaymentWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace EntertainmentApp
 {
@@ -19,7 +9,7 @@ namespace EntertainmentApp
     /// Interaction logic for CountPaymentWindow.xaml
     /// </summary>
 
-    
+
     public partial class CountPaymentWindow : Window
     {
         public ObservableCollection<int> Numbers { get; set; }
@@ -50,7 +40,16 @@ namespace EntertainmentApp
 
         private void printReceiptButton_Click(object sender, RoutedEventArgs e)
         {
-
+            if (hoursComboBox.SelectedValue == null || guestComboBox.SelectedValue == null)
+                ErrorMessage.Content = "Błąd, brak wyboru gościa lub godzin!";
+            else
+            {
+                serviceLabel.Content = "- " + innEnt.name;
+                priceLabel2.Content = priceLabel.Content + " zł";
+                fullNameLabel.Content = guestComboBox.Text;
+                ReceiptPopup.IsOpen = true;
+            }
+                
         }
 
         private void hoursComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -59,6 +58,16 @@ namespace EntertainmentApp
             bool parseOK = Int32.TryParse(hoursComboBox.SelectedValue.ToString(), out hours);
             float overallPrice = innEnt.price * hours;
             priceLabel.Content = overallPrice;
+        }
+
+        private void guestComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ReceiptPopup.IsOpen = false;
         }
     }
 }
